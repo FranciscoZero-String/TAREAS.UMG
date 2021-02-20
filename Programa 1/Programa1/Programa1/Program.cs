0a4bcd7fa43e546c48438cccda75980755c09bbe
FranciscoZero-String <frnacisol@gmail.com>
2021-02-20 13:36:38 -0600

    Subiendo primeros programas

---
+++ Programa 1/Programa1/Programa1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace programa1
+
+{ 
+    public class Program
+
+    {  
+        public static void Main(String[]args)
+
+        {
+
+            String c;
+            Console.WriteLine("Ingrese una letra (c,C)");
+            c = Console.ReadLine();
+            if(c=="C")
+            {
+                Console.Write("Es mayuscula");
+            }
+            {
+                Console.Write("Es minuscula");
+
+            }
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
