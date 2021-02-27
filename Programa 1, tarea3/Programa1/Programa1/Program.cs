05fb29f305511d6ddf712454b793a0a07b7ef765
FranciscoZero-String <79378583+FranciscoZero-String@users.noreply.github.com>
2021-02-26 19:56:30 -0600

    Add files via upload

---
+++ Programa 1, tarea3/Programa1/Programa1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Programa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Ingrese cualquier numero");
+            numero = int.Parse(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+            }
+
+            else
+            {
+                Console.WriteLine("El numero no es par");
+            }
+        
+        }
+    }
+}
