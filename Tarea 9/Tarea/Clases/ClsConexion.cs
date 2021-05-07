b8cb07d38a6db97b859c514ccf3127042c8b61fd
FranciscoZero-String <79378583+FranciscoZero-String@users.noreply.github.com>
2021-05-07 14:28:19 -0600

    Add files via upload

---
+++ Tarea 9/Tarea/Clases/ClsConexion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Tarea.Clases
+{
+    public class ClsConexion
+    {
+        Conexion Conectionc = new Conexion();
+
+        public DataTable MostrarDatos()
+        {
+            SqlDataAdapter datosU = new SqlDataAdapter("SP_MOSTRARtb_alumnos",Conectionc.LeerCadena());
+            datosU.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            datosU.Fill(dt);
+            return dt;
+        }
+
+    }
+}
---
+++ Tarea 9/Tarea/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Tarea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Prueba1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Prueba1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Prueba1
+            // 
+            this.Prueba1.AllowUserToAddRows = false;
+            this.Prueba1.AllowUserToDeleteRows = false;
+            this.Prueba1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Prueba1.Location = new System.Drawing.Point(122, 101);
+            this.Prueba1.Name = "Prueba1";
+            this.Prueba1.ReadOnly = true;
+            this.Prueba1.Size = new System.Drawing.Size(534, 290);
+            this.Prueba1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(313, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Base de Datos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Prueba1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Prueba1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Prueba1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
