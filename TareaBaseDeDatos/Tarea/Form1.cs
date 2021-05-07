71b3de3b573149e5bd635326f2d5fc0c2462c0a1
FranciscoZero-String <79378583+FranciscoZero-String@users.noreply.github.com>
2021-05-07 14:47:38 -0600

    Add files via upload

---
+++ TareaBaseDeDatos/Tarea/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tarea.Clases;
+
+namespace Tarea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ClsConexion Datos = new ClsConexion();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Prueba1.DataSource = Datos.MostrarDatos();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
